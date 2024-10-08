a67fe6b71552f5bc591bee8e036f9e8c29107c65
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-10-08 09:38:41 +0200

    v0.4.40

--- settings.lua
+++ settings.lua
@@ -54,14 +54,14 @@ end
 
 function ThreatMeter:InitSettings()
 	TMTAB = TMTAB or {}
-	ThreatMeter:SetVersion(AddonName, 132117, "0.4.39")
+	ThreatMeter:SetVersion(AddonName, 132117, "0.4.40")
 	tm_settings = ThreatMeter:CreateFrame(
 		{
 			["name"] = "ThreatMeter",
 			["pTab"] = {"CENTER"},
 			["sw"] = 520,
 			["sh"] = 520,
-			["title"] = format("ThreatMeter |T132117:16:16:0:0|t v|cff3FC7EB%s", "0.4.39")
+			["title"] = format("ThreatMeter |T132117:16:16:0:0|t v|cff3FC7EB%s", "0.4.40")
 		}
 	)
 
@@ -127,7 +127,7 @@ frame:SetScript(
 							["icon"] = 132117,
 							["var"] = mmbtn,
 							["dbtab"] = TMTAB,
-							["vTT"] = {{"ThreatMeter |T132117:16:16:0:0|t", "v|cff3FC7EB0.4.39"}, {"Leftclick", "Open Settings"}, {"Rightclick", "Unlock/lock Text"}, {"Shift + Rightclick", "Hide Minimap Icon"}},
+							["vTT"] = {{"ThreatMeter |T132117:16:16:0:0|t", "v|cff3FC7EB0.4.40"}, {"Leftclick", "Open Settings"}, {"Rightclick", "Unlock/lock Text"}, {"Shift + Rightclick", "Hide Minimap Icon"}},
 							["funcL"] = function()
 								ThreatMeter:ToggleSettings()
 							end,
